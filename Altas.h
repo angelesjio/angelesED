456ceb9f3991ee187f2742d53384a36bcd0de688
JioniWAng <a8759551@gmail.com>
2026-06-16 08:57:40 -0600

    Add files via upload

---
+++ Altas.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+    struct Persona *Ptr;
+
+    void Altas (struct Persona **Ptr);
+
+    void Ingresar (struct Persona **Ptr){
+        struct Persona *Ptraux;
+
+        printf("~~Ingrese datos~~\n");
+        if ()
+    }
