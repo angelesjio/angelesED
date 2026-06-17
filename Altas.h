6c1043086cf89a6fa5d2a3f463877e240e34bc94
JioniWAng <a8759551@gmail.com>
2026-06-17 07:38:09 -0600

    Actualizacion de altas, faltan opciones

--- Altas.h
+++ Altas.h
@@ -6,6 +6,15 @@
 
     void Ingresar (struct Persona **Ptr){
         struct Persona *Ptraux;
+        int opcion; 
+        char nombre [ 50 ]; 
+        Ptraux-> nombre= (char*) malloc (sizeof (stilen(nombre)+1)*char);
+        printf ("Ingrese el nombre: ");
+        scanf ("%s", &Ptraux->nombre);
+
+        printf ("\nEs alumno (1) o Personal (2)\n");
+        scanf ("%d", &opcion);
+        
 
         printf("~~Ingrese datos~~\n");
         if ()
