dcb08894755fb55c757bc4e152a4af6981a9ab28
JioniWAng <a8759551@gmail.com>
2026-06-26 13:03:11 -0600

    Update Bajas.h

--- Bajas.h
+++ Bajas.h
@@ -2,116 +2,112 @@
 #include <stdlib.h>
 #include "estructuras.h"
 
-    int Baja (struct Persona **Ptr);
-    int VariasBajas (struct Persona **Ptr);
-    int Eliminartodo (struct Persona **Ptr);
+int Baja (struct Persona **Ptr);
+int VariasBajas (struct Persona **Ptr);
+int Eliminartodo (struct Persona **Ptr);
 
-    int Baja (struct Persona **Ptr){
-        struct Persona *Ptrauxiliar=*Ptr;
-        struct Persona *Antes=NULL;
-        int b=1;
+int Baja (struct Persona **Ptr){
 
-        if (Ptr==NULL){
-            printf ("No se ha reservado memoria\n");
-            b=0; 
-        }else{
-            Ptrauxiliar=*Ptr;
-            while(Ptrauxiliar->Ptrsig!= NULL){
-                Ptrauxiliar=Ptrauxiliar->Ptrsig;
+    struct Persona *Ptrauxiliar=*Ptr;
+    struct Persona *Antes=NULL;
 
-                if ((Ptrauxiliar->Ptrsig)->Ptrsig==NULL){
-                    Antes=(Ptrauxiliar->Ptrsig)->Ptrsig;
-                }
-            }
-            
-            Antes->Ptrsig=NULL;
-            free(Ptrauxiliar->nombre);
-            free (Ptrauxiliar->PtrAlum);
-            free (Ptrauxiliar->Ptrsig);
-            free(Ptrauxiliar);
+    int b=1;
 
-            b=1;
-            
-        }
-
+    if (Ptrauxiliar==NULL){
+        printf ("No se ha reservado memoria\n");
+        b=0; 
+    }else{
+        Ptrauxiliar=*Ptr;
         
-      return b;
+        *Ptr=(*Ptr)->Ptrsig;
+        
+        free(Ptrauxiliar->nombre);//NO SE
+        free (Ptrauxiliar->PtrAlum);
+        free (Ptrauxiliar->Ptrsig);
+        free(Ptrauxiliar);
+
+        b=1;
+        
     }
 
-    int VariasBajas (struct Persona **Ptr){
+    
+    return b;
+}
 
-        struct Persona *Ptrauxiliar=*Ptr;
-        struct Persona *Antes=NULL;
-        int b=1;
-        int opB;
+int VariasBajas (struct Persona **Ptr){
+
+    struct Persona *Ptrauxiliar=*Ptr;
+    struct Persona *Antes=NULL;
+    int b=1;
+    int opB;
+
+    do{
+        if (Ptrauxiliar==NULL){
+        printf ("No se ha reservado memoria\n");
+        b=0; 
+    }else{
+        Ptrauxiliar=*Ptr;
+        while(Ptrauxiliar->Ptrsig!= NULL){
+            Ptrauxiliar=Ptrauxiliar->Ptrsig;
+
+            if ((Ptrauxiliar->Ptrsig)->Ptrsig==NULL){
+                Antes=(Ptrauxiliar->Ptrsig)->Ptrsig;
+            }
+        }
+        
+        Antes->Ptrsig=NULL;
+        free(Ptrauxiliar->nombre);
+        free (Ptrauxiliar->PtrAlum);
+        free (Ptrauxiliar->Ptrsig);
+        free(Ptrauxiliar);
+
+        b=1;
+        
+    }
+
+    printf ("\n Desea seguir eliminando? (1) Si, (0) No\n");
+    scanf ("%d", &opB);
+    } while(opB!=0);
+
+    return b; 
+
+}
+
+int Eliminartodo (struct Persona **Ptr){
+    struct Persona *Ptrauxiliar=*Ptr;
+    struct Persona *Antes=NULL;
+    int b=1;
+    int opB;
+
+    
+    if (Ptr==NULL){
+        printf ("No hay nada que aliminar\n");
+        b=0; 
+
+    }else{
 
         do{
-            if (Ptr==NULL){
-            printf ("No se ha reservado memoria\n");
-            b=0; 
-        }else{
             Ptrauxiliar=*Ptr;
-            while(Ptrauxiliar->Ptrsig!= NULL){
-                Ptrauxiliar=Ptrauxiliar->Ptrsig;
+        while(Ptrauxiliar->Ptrsig!= NULL){
+            Ptrauxiliar=Ptrauxiliar->Ptrsig;
 
-                if ((Ptrauxiliar->Ptrsig)->Ptrsig==NULL){
-                    Antes=(Ptrauxiliar->Ptrsig)->Ptrsig;
-                }
+            if ((Ptrauxiliar->Ptrsig)->Ptrsig==NULL){
+                Antes=(Ptrauxiliar->Ptrsig)->Ptrsig;
             }
-            
-            Antes->Ptrsig=NULL;
-            free(Ptrauxiliar->nombre);
-            free (Ptrauxiliar->PtrAlum);
-            free (Ptrauxiliar->Ptrsig);
-            free(Ptrauxiliar);
-
-            b=1;
-            
         }
-
-        printf ("\n Desea seguir eliminando? (1) Si, (0) No\n");
-        scanf ("%d", &opB);
-        } while(opB!=0);
-
-        return b; 
-
-    }
-
-    int Eliminartodo (struct Persona **Ptr){
-        struct Persona *Ptrauxiliar=*Ptr;
-        struct Persona *Antes=NULL;
-        int b=1;
-        int opB;
-
         
-        if (Ptr==NULL){
-            printf ("No hay nada que aliminar\n");
-            b=0; 
+        Antes->Ptrsig=NULL;
+        free(Ptrauxiliar->nombre);
+        free (Ptrauxiliar->PtrAlum);
+        free (Ptrauxiliar->Ptrsig);
+        free(Ptrauxiliar);
 
-        }else{
+        b=1;
 
-            do{
-                Ptrauxiliar=*Ptr;
-            while(Ptrauxiliar->Ptrsig!= NULL){
-                Ptrauxiliar=Ptrauxiliar->Ptrsig;
-
-                if ((Ptrauxiliar->Ptrsig)->Ptrsig==NULL){
-                    Antes=(Ptrauxiliar->Ptrsig)->Ptrsig;
-                }
-            }
-            
-            Antes->Ptrsig=NULL;
-            free(Ptrauxiliar->nombre);
-            free (Ptrauxiliar->PtrAlum);
-            free (Ptrauxiliar->Ptrsig);
-            free(Ptrauxiliar);
-
-            b=1;
-
-            }while(Ptr!=NULL);
-            
-        }
-
-        return b;
-
+        }while(Ptr!=NULL);
+        
     }
+
+    return b;
+
+}
